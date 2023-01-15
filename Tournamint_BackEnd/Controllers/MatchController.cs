ca814bf5ba0c9c63b3d8dde98eeb8abb1c10af6b
tomasvasaris <106983996+tomasvasaris@users.noreply.github.com>
2023-01-15 20:19:41 +0200

    Clean up
    Deleted unnecessary parts that I've copied from template

--- Tournamint_BackEnd/Controllers/MatchController.cs
+++ Tournamint_BackEnd/Controllers/MatchController.cs
@@ -8,6 +8,31 @@ namespace Tournamint_BackEnd.Controllers
     [ApiController]
     public class MatchController : ControllerBase
     {
+        private readonly ILogger<MatchController> _logger;
+
+        public MatchController(ILogger<MatchController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogCritical("Critical info from logger");
+            _logger.LogError("Error  info from logger");
+            _logger.LogWarning("Warning");
+
+            _logger.LogInformation("Info from logger");
+            _logger.LogDebug("Debug info from logger");
+            _logger.LogTrace("Trace");
+
+            return Ok();
+        }
+
+
+
+
+
         List<Match> matches = new List<Match> { };
 
         [HttpGet("matches/{id}")]
---
+++ Tournamint_BackEnd/Models/DTO/GetMatchResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Tournamint_BackEnd.Models.DTO
+{
+    public class GetMatchResult
+    {
+        public GetMatchResult(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+    }
+}
