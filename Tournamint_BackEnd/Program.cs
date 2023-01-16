7463bf6d451799129e2a58d7c51887332e0597ca
tomasvasaris <106983996+tomasvasaris@users.noreply.github.com>
2023-01-16 11:38:02 +0200

    Clean up before branching
    Removing some comments, unnecessary additions. Preparing for FE and BE integration

--- Tournamint_BackEnd/Program.cs
+++ Tournamint_BackEnd/Program.cs
@@ -31,27 +31,6 @@ namespace Tournamint_BackEnd
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 option.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
-
-                /*
-                var securityScheme = new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header is using Bearer scheme. \r\n\r\n" +
-                        "Enter token. \r\n\r\n" +
-                        "Example: \"d5f41g85d1f52a\"",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.Http,
-                    Scheme = "bearer",
-                    BearerFormat = "JWT",
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                };
-                option.AddSecurityDefinition("Bearer", securityScheme);
-                option.AddSecurityRequirement(new OpenApiSecurityRequirement { { securityScheme, new[] { "Bearer" } } });
-                */
             });
 
             var app = builder.Build();
